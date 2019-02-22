8f20223d690b51bb6436516d245d4a0e9cbe0d21
3yebMB <3yebmb@gmail.com>
2019-02-22 10:33:59 +0500

    example how singleton works

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class MainClass {
+    public static void main(String[] args) {
+
+        Singleton x = Singleton.getInstance();
+
+        Singleton y = Singleton.getInstance();
+
+        Singleton z = Singleton.getInstance();
+
+        x.s = (x.s).toUpperCase();
+
+        System.out.println("String from x is " + x.s);
+        System.out.println("String from y is " + y.s);
+        System.out.println("String from z is " + z.s);
+        System.out.println("\n");
+
+        z.s = (z.s).toLowerCase();
+
+        System.out.println("String from x is " + x.s);
+        System.out.println("String from y is " + y.s);
+        System.out.println("String from z is " + z.s);
+    }
 }
